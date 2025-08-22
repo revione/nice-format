7545c08ff744aa09365031edd0e5f891afada63d
revione <revione@outlook.com>
2025-08-22 17:32:07 +0200

    toggle dark

---
+++ tipo-de-palabras/front/toggleDark.js
@@ -0,0 +1,10 @@
+const app = () => {
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "1" && event.metaKey) {
+      event.preventDefault();
+      document.body.classList.toggle("dark");
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", app);
